5e76dc0dd88b2c5bb3bfccc090668b0315c32334
Camille Ferrell <ferrell.c@northeastern.edu>
2026-03-30 19:09:04 -0400

    added an updated HLS code that we should try to test (doesn't cast pointers), and and new PYNQ test as well as a final PYNQ code to match the new HLS

--- demodulate_debugging.cpp
+++ demodulate_debugging.cpp
@@ -39,3 +39,46 @@ void demodulate(
         output_signal.write(out_val);
     }
 }
+
+
+// *********************** UPDATED VERSION THAT WE SHOULD TRY ON WED ********************
+#include <hls_stream.h>
+#include <ap_axi_sdata.h>
+#include <ap_int.h>
+
+typedef ap_axiu<32, 0, 0, 0> axis_t;
+
+union float_bits {
+    float f;
+    ap_uint<32> u;
+};
+
+void demodulate(
+    hls::stream<axis_t> &input_signal,
+    hls::stream<axis_t> &carrier,
+    hls::stream<axis_t> &output_signal
+) {
+#pragma HLS INTERFACE axis port=input_signal
+#pragma HLS INTERFACE axis port=carrier
+#pragma HLS INTERFACE axis port=output_signal
+#pragma HLS INTERFACE s_axilite port=return bundle=control
+
+    for (int i = 0; i < 1024; i++) {
+#pragma HLS PIPELINE II=1
+        axis_t in_val = input_signal.read();
+        axis_t car_val = carrier.read();
+        axis_t out_val;
+
+        float_bits a, b, y;
+        a.u = in_val.data;
+        b.u = car_val.data;
+        y.f = a.f * b.f;
+
+        out_val.data = y.u;
+        out_val.keep = in_val.keep;
+        out_val.strb = in_val.strb;
+        out_val.last = in_val.last;
+
+        output_signal.write(out_val);
+    }
+}
